f44168f0976d2a7a2162a9c115dc94728961f32d
JoeKelly1107 <josephkelly1107@gmail.com>
2023-04-18 16:53:38 -0400

    animations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
@@ -21,3 +23,20 @@ document.addEventListener('click', e => {
     })
 
 })
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
